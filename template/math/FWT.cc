7619a5bbc721c037f46108960a6d5581cb5a8607
jufusong <jufusong@gmail.com>
2017-10-03 18:52:12 +0800

    auto commit

--- template/math/FWT.cc
+++ template/math/FWT.cc
@@ -11,8 +11,8 @@ int inverse(int x) {
 
 void FWT(vector<int> &a) {
   int n = a.size();
-  for (int d = 1; d < n; d <<= 1)
-    for (int m = d << 1, i = 0; i < n; i += m)
+  for (int d = 1; d < n; d <<= 1) {
+    for (int m = d << 1, i = 0; i < n; i += m) {
       for (int j = 0; j < d; j++) {
         int x = a[i + j], y = a[i + j + d];
         a[i + j] = (x + y) % mod, a[i + j + d] = (x - y + mod) % mod;
@@ -20,13 +20,15 @@ void FWT(vector<int> &a) {
         // and : a[i + j] = x + y;
         // or  : a[i + j + d] = x + y;
       }
+    }
+  }
 }
 
 void IFWT(vector<int> &a) {
   int n = a.size();
   int inv2 = inverse(2);
-  for (int d = 1; d < n; d <<= 1)
-    for (int m = d << 1, i = 0; i < n; i += m)
+  for (int d = 1; d < n; d <<= 1) {
+    for (int m = d << 1, i = 0; i < n; i += m) {
       for (int j = 0; j < d; j++) {
         int x = a[i + j], y = a[i + j + d];
         a[i + j] = 1LL * (x + y) * inv2 % mod;
@@ -35,4 +37,6 @@ void IFWT(vector<int> &a) {
         // and : a[i + j] = x - y;
         // or  : a[i + j + d] = y - x;
       }
+    }
+  }
 }
---
+++ template/math/Inverse.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int inverse(int a, int mod) {
+  a %= mod;
+  if (a < 0) a += mod;
+  int b = mod, u = 0, v = 1;
+  while (a) {
+    int t = b / a;
+    b -= t * a; swap(a, b);
+    u -= t * v; swap(u, v);
+  }
+  assert(b == 1);
+  if (u < 0) u += mod;
+  return u;
+}
